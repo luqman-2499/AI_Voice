026c083b5e2ae6c4637aae250a175c1b03940c7c
luqman-2499 <khan4luqman@gmail.com>
2025-10-30 01:18:21 +0530

    updated controller allow cross site access

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,9 +26,9 @@ import bcrypt from "bcryptjs"
 
         res.cookie("token",token, {
             httpOnly: true,
-            maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false 
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "none",
+            secure: true 
         })
 
         return res.status(201).json(user)
