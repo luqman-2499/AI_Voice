50bc4cc2648d675ac9549c816f3788d244985ba3
luqman-2499 <khan4luqman@gmail.com>
2025-10-30 01:25:14 +0530

    updated controller allow cross site access

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,9 +61,9 @@ export const login = async (req,res) => {
 
         res.cookie("token",token, {
             httpOnly: true,
-            maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false 
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "none",
+            secure: true 
         }) 
 
         return res.status(201).json(user)
