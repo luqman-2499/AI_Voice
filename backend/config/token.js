74391443218d76d4ad0659d7346ad7b82ebf7870
luqman-2499 <khan4luqman@gmail.com>
2025-11-01 20:31:06 +0530

    testing 2 for deployment

--- backend/config/token.js
+++ backend/config/token.js
@@ -1,14 +1,35 @@
-import jwt from "jsonwebtoken"
+// import jwt from "jsonwebtoken"
 
-const genToken = async (userId) => {
+// const genToken = async (userId) => {
+//     try {
+//         const token = jwt.sign({userId},process.env.JWT_SECRET, {expiresIn:"10d"})
+
+//         return token
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+// };
+
+// export default genToken
+
+
+
+const genTokenAndSetCookie = async (userId, res) => {
     try {
-        const token = jwt.sign({userId},process.env.JWT_SECRET, {expiresIn:"10d"})
+        const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn:"10d"})
+        
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', // HTTPS in production
+            sameSite: 'none', // Cross-site cookies
+            maxAge: 10 * 24 * 60 * 60 * 1000 // 10 days
+        });
 
-        return token
+        return token;
     } catch (error) {
         console.log(error);
-        
     }
 };
 
-export default genToken
+export default genTokenAndSetCookie;
